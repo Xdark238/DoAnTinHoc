0690063ae87d1565b15b2a930d2ad26688f9fef5
larryking59 <larryking.n95@gmail.com>
2017-05-09 19:03:11 +0700

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTS_TTT
+{
+    public class Game
+    {
+        public byte[,] board = new byte[11,9]; 
+        public void Restart()
+        {
+            board = new byte[11,9];
+        }
+
+        public void Mark(int player, byte indexX,byte indexY)
+        {
+            if (board[indexY,indexX] != 0)
+                throw new Exception("Tryed to mark an non empty slot");
+
+            if (player == 1)
+                board[indexY,indexX] = 1;
+            else
+                board[indexY,indexX] = 2;
+        }
+
+        public List<byte> GetValidMoves()
+        {
+            List<byte> moves = new List<>();
+
+            for (int i = 0; i < 9; i++ )
+                for (int j=0;j<11;j++)
+                    if (board[j,i] == 0)
+                        moves.Add((byte)i,j);    
+            return moves;
+        }
+
+        public List<byte> GetValidMoves(byte[,] state)
+        {
+            List<byte> moves = new List<byte[,]>();
+
+            for (int i = 0; i < 9; i++)
+                for(int j=0;i<11;j++)
+                 if (state[j,i] == 0)
+                    moves.Add((byte)i,j);
+
+            return moves;
+        }
+
+        public int GetWinner()
+        {
+           /*
+            * 
+            * 
+            * 
+            */
+        }
+
+        public int GetWinner(byte[,] board)
+        {
+           /*
+            * 
+            * 
+            * 
+            */
+        }
+
+        public bool IsTerminal(byte[,] state)
+        {
+            return GetWinner(state) != 0;
+        }
+
+        public void Draw()
+        {
+           /*
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < 3; i++)
+                for (int j=0;j<11;j++)
+                Console.Write(board[j,i].ToString() + "-");
+
+            Console.Write("\r\n");
+
+            for (int i = 3; i < 6; i++)
+                for (int j=0;j<11;j++)
+                Console.Write(board[j,i].ToString() + "-");
+
+            Console.Write("\r\n");
+
+            for (int i = 6; i < 9; i++)
+                for (int j=0;j<11;j++)
+                Console.Write(board[j,i].ToString() + "-");
+
+            Console.WriteLine("\r\n--------------------");
+            */
+        }
+    }
+}
---
+++ MemCpy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTS_TTT
+{
+    unsafe public class MemoryCopy
+    {
+        public delegate void MemCpyFunction(void* des, void* src, uint bytes);
+        public readonly MemCpyFunction MemCpy;
+
+        public MemoryCopy()
+        {
+            #region generate most efficient memory copy
+            var dynamicMethod = new DynamicMethod
+             (
+                 "MemCpy",
+                 typeof(void),
+                 new[] { typeof(void*), typeof(void*), typeof(uint) },
+                 typeof(Program)
+             );
+
+            var ilGenerator = dynamicMethod.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.Emit(OpCodes.Ldarg_2);
+
+            ilGenerator.Emit(OpCodes.Cpblk);
+            ilGenerator.Emit(OpCodes.Ret);
+
+            MemCpy = (MemCpyFunction)dynamicMethod
+                        .CreateDelegate(typeof(MemCpyFunction));
+            #endregion
+        }
+           
+    }
+}
